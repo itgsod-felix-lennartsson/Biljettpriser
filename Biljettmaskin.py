3ee4ce2cd81e180309d8174c10e0a0958c611d7a
itgsod-felix-lennartsson <felix.lennartsson@elev.it-gymnasiet.se>
2015-09-15 10:42:05 +0200

    Biljettmaskin 1.1
    + Utförlig dokumentation

--- Biljettmaskin.py
+++ Biljettmaskin.py
@@ -1,7 +1,7 @@
 from data.functions import priskoll
 
-price = priskoll(raw_input("Var god ange alder: "))
-if price == "err":
+price = priskoll(raw_input("Var god ange alder: ")) # Tar input från användare om dess ålder och skickar vidare till functions/priskoll.
+if price == "err": # Kollar om errormeddelande för att åldern inte är 0-130 år är givet.
     print("ERROR: Alder maste vara mellan 0 - 130 ar!")
-else:
-    print "Din biljett kostar: " + str(price) + " kr."
+else: # Kod nedan körs förutsatt att error ovan inte är givet.
+    print "Din biljett kostar: " + str(price) + " kr." # Skriver ut kostnaden för användarens biljett. Pris förvaras i price, vilken defineras av functions/priskoll ovan.
--- data/functions.py
+++ data/functions.py
@@ -1,8 +1,8 @@
 def priskoll(age):
-    age = int(age)
-    if age in range(18,65):
+    age = int(age)   # Omvandlar input str till int
+    if age in range(18,65): # Returnerar 20 kr i biljettpris då användare är mellan 18-64 år gammal.
         return 20
-    elif age in range(0,131):
+    elif age in range(0,131): # Returnerar 15 kr i biljettpris till alla övriga användare. Användaren måste vara mellan 0-130 år.
         return 15
     else:
-        return "err"
+        return "err" # Returnerar variabel som behandlas som felkod som ger information om att användaren har angivit ålder som inte är mellan 0-130 år.
